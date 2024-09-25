9e37e0f25bc3924d5fff28bf674ff6cc4df37b2f
CrazuSmile <juanjosesanzlopez@gmail.com>
2024-09-25 11:55:19 +0200

    Cambio de directorio

--- 1st term/UD1 Activity 2 - Modules in NodeJS/exercise1/greeting.js
+++
@@ -1,3 +0,0 @@
-const os = require('node:os');
-var hostname = os.userInfo()
-console.log("Hello "+hostname.username)
--- 1st term/UD1 Activity 2 - Modules in NodeJS/exercise2/index.js
+++
@@ -1,6 +0,0 @@
-const lodash = require('lodash');
-
-var arrayNames = lodash;
-let names = ["Juan Jose", "Jose Miguel", "Miguel"];
-
-console.log(arrayNames.join(names, ','))
