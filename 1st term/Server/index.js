080a1013f0d3b3c93b44f021090f0ef92bfc19c6
CrazuSmile <juanjosesanzlopez@gmail.com>
2024-09-27 11:51:00 +0200

    Cambio JSON

--- 1st term/Server/index.js
+++ 1st term/Server/index.js
@@ -2,8 +2,15 @@ const http = require('http');
 
 var server = http.createServer(function (req, res) {
     console.log(req.url);
-    res.writeHead(200, { 'Content-type': 'text/plain' });
-    res.end('Hello World');
+    res.writeHead(200, { 'Content-type': 'application/json' });
+
+    var miObj = {
+        brand: 'Ford',
+        model: 'Focus',
+        license_plate: 1232312
+    }
+
+    res.end(JSON.stringify(miObj));
 });
 
 server.listen(3000, '127.0.0.1');
