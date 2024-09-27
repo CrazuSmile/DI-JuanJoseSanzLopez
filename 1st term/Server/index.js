57d7aa89e4d9b922bbf66920196377940c693b59
CrazuSmile <juanjosesanzlopez@gmail.com>
2024-09-27 11:37:52 +0200

    Server example

---
+++ 1st term/Server/index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+var server = http.createServer(function (req, res) {
+    console.log(req.url);
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.end('Hello World');
+});
+
+server.listen(3000, '127.0.0.1');
