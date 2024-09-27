e8e75e137273a3898555944291ac3d3374017c09
CrazuSmile <juanjosesanzlopez@gmail.com>
2024-09-27 11:57:48 +0200

    Creacion de users.json y su uso

--- 1st term/Server/index.js
+++ 1st term/Server/index.js
@@ -1,16 +1,19 @@
-const http = require('http');
+// const http = require('http');
+// 
+// var server = http.createServer(function (req, res) {
+//     console.log(req.url);
+//     res.writeHead(200, { 'Content-type': 'application/json' });
+// 
+//     var miObj = {
+//         brand: 'Ford',
+//         model: 'Focus',
+//         license_plate: 1232312
+//     }
+// 
+//     res.end(JSON.stringify(miObj));
+// });
+// 
+// server.listen(3000, '127.0.0.1');
 
-var server = http.createServer(function (req, res) {
-    console.log(req.url);
-    res.writeHead(200, { 'Content-type': 'application/json' });
-
-    var miObj = {
-        brand: 'Ford',
-        model: 'Focus',
-        license_plate: 1232312
-    }
-
-    res.end(JSON.stringify(miObj));
-});
-
-server.listen(3000, '127.0.0.1');
+const users = require("./users.json");
+console.log(users);
