8217fd85477ba689ff78f30347ce012c5b496157
CrazuSmile <juanjosesanzlopez@gmail.com>
2024-09-27 11:59:20 +0200

    Uso de "fs"

--- 1st term/Server/index.js
+++ 1st term/Server/index.js
@@ -15,5 +15,17 @@
 // 
 // server.listen(3000, '127.0.0.1');
 
-const users = require("./users.json");
-console.log(users);
+// const users = require("./users.json");
+// console.log(users);
+
+const fs = require("fs");
+
+// Read users.json file
+fs.readFile("users.json", function (err, data) {
+    // Check for errors
+    if (err) throw err;
+    
+    // Converting to JSON
+    const users = JSON.parse(data);
+    console.log(users); // Print users
+});
