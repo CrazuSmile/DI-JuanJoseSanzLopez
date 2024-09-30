deb602084ed04e9bfa3e2ab58682817cfa1b2c8b
CrazuSmile <juanjosesanzlopez@gmail.com>
2024-09-30 10:30:13 +0200

    Ejemplo de mongo

---
+++ 1st term/UD1/MongoDB/app.js
@@ -0,0 +1,54 @@
+// we load moongose
+const mongoose = require('mongoose');
+// we set the default promise engine to javascript
+// this is necessary because mongo supports different types of promises
+mongoose.Promise = global.Promise;
+// connect with the contacts DB
+mongoose.connect('mongodb://localhost:27017/contacts', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//scheme
+let contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    telephone: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: /^\d{9}$/
+    },
+    age: {
+        type: Number,
+        min: 18,
+        max: 120
+    }
+});
+
+//model
+let Contact = mongoose.model('contacts', contactSchema);
+
+// add documents
+// first we create the contact
+let contact1 = new Contact({
+    name: "Boris",
+    telephone: "946112230",
+    age: 49
+});
+// we will use a save promise to save it to the database
+let p1 = contact1.save().then(result => {
+    console.log("Contact added:", result);
+}).catch(error => {
+    console.log("ERROR adding contact:", error);
+});
+// we must wait for all promises to finish, since they are asynchronous
+// to close the connection to the database, Promise.all allows to query the // promises passed as a parameter to see if they have finished:
+Promise.all([p1]).then(values => {
+    mongoose.connection.close();
+});
